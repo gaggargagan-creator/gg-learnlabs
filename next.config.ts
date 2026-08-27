a62c7b266cbfbb963c84467be82bd23366e468c6
gaggargagan-creator <gaggargagan@gmail.com>
2026-08-27 20:17:20 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
   basePath: "/gg-learnlabs",
+
+  assetPrefix: "/gg-learnlabs/",
+
   trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
