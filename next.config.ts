fadce6dee73c1d79e26c3b0a3fb3820e734d0dd2
gaggargagan-creator <gaggargagan@gmail.com>
2026-08-27 19:03:40 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/gg-learnlabs",
+  trailingSlash: true,
+};
+
+export default nextConfig;
